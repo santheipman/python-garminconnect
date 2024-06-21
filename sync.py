db10cfd1f36779bd077b188fcd0a785f40b53374
sanvuqui <vqusan@gmail.com>
2024-06-22 02:03:57 +0700

    added refresh access token, read from secret, sync list of activities,...

--- sync.py
+++ sync.py
@@ -1,55 +1,101 @@
+import json
+import time
+from dataclasses import dataclass
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
+from dotenv import load_dotenv
 
 from example import init_api, email, password
 
+strava_token_filepath = os.path.expanduser('~/.strava_token')
+
+
+@dataclass
+class StravaToken:
+    access_token: str
+    refresh_token: str
+    expires_at: int
+
+
+def load_strava_token() -> StravaToken:
+    try:
+        with open(strava_token_filepath, 'r') as f:
+            token_data = json.load(f)
+            token = StravaToken(
+                access_token=token_data['access_token'],
+                refresh_token=token_data['refresh_token'],
+                expires_at=token_data['expires_at']
+            )
+            print(f"Token loaded from {strava_token_filepath}")
+            return token
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: File not found at {strava_token_filepath}")
+
 
 class Synchronizer:
-    def __init__(self, access_token, refresh_token):
-        self.access_token = access_token
-        self.refresh_token = refresh_token
+    def __init__(self, token: StravaToken):
+        self.token = token
 
     @staticmethod
-    def get_garmin_activities(start, limit):
+    def get_garmin_activities(start: int, limit: int) -> list[dict]:
         api = init_api(email, password)
-        activities = api.get_activities(start, limit)
-        for activity in activities:
-            print(f"start time: {activity['startTimeGMT']}")
-            d = datetime.strptime(activity["startTimeGMT"], "%Y-%m-%d %H:%M:%S")
-            print(f"d: {d}")
 
-        return activities
+        return api.get_activities(start, limit)
 
-    def refresh_token(self):
-        return # TODO
+    def save_strava_token(self):
+        with open(strava_token_filepath, 'w') as f:
+            json.dump({
+                'access_token': self.token.access_token,
+                'refresh_token': self.token.refresh_token,
+                'expires_at': self.token.expires_at
+            }, f)
+        print(f"Token saved to {strava_token_filepath}")
 
-    def get_strava_activities(self, after):
+    def check_refresh_access_token(self):
+        if time.time() < self.token.expires_at - 20:
+            return
+
         try:
+            data = {
+                'client_id': os.getenv("GARSYNC_CLIENT_ID"),
+                'client_secret': os.getenv("GARSYNC_CLIENT_SECRET"),
+                'refresh_token': self.token.refresh_token,
+                'grant_type': 'refresh_token'
+            }
+
+            response = requests.post("https://www.strava.com/oauth/token", data=data)
+            response.raise_for_status()
+            response_json = response.json()
+            self.token = StravaToken(response_json["access_token"], response_json["refresh_token"],
+                                     response_json["expires_at"])
+            self.save_strava_token()
+
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError(f"Request failed: {e}")
+
+    def get_strava_activities(self, after: int) -> list[dict]:
+        try:
+            self.check_refresh_access_token()
+
             headers = {
-                "Authorization": f"Bearer {self.access_token}"
+                "Authorization": f"Bearer {self.token.access_token}"
             }
             url = f"https://www.strava.com/api/v3/athlete/activities?after={after}"
             response = requests.get(url, headers=headers)
             response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-            activities = response.json()
+            return response.json()
 
-            # Print the activities start time
-            for activity in activities:
-                print(f"Start time: {activity['start_date']}")
-                d = datetime.strptime(activity["start_date"], "%Y-%m-%dT%H:%M:%SZ")
-                print(f"d: {d}")
-
-            return activities
         except requests.exceptions.RequestException as e:
-            print(f"Request failed: {e}")
-            return None
+            raise RuntimeError(f"Request failed: {e}")
 
-    def set_strava_activity_name(self, activity_id, name):
+    def set_strava_activity_name(self, activity_id: str, name: str):
         try:
+            self.check_refresh_access_token()
+
             headers = {
-                "Authorization": f"Bearer {self.access_token}",
+                "Authorization": f"Bearer {self.token.access_token}",
                 "Content-Type": "application/json"
             }
             data = {
@@ -61,24 +107,30 @@ class Synchronizer:
 
             return
         except requests.exceptions.RequestException as e:
-            print(f"Request failed: {e}")
-            return
+            raise RuntimeError(f"Request failed: {e}")
 
-    def sync_activity_name_from_garmin_to_strava(self, garmin_activities, strava_activities):
+    def sync_activity_name_from_garmin_to_strava(self, garmin_activities: list[dict], strava_activities: list[dict]):
         for strava_activity in strava_activities:
             strava_start = datetime.strptime(strava_activity["start_date"], "%Y-%m-%dT%H:%M:%SZ")
+            strava_name = strava_activity["name"]
             for garmin_activity in garmin_activities:
                 garmin_start = datetime.strptime(garmin_activity["startTimeGMT"], "%Y-%m-%d %H:%M:%S")
-                if strava_start == garmin_start:
-                    garmin_name = garmin_activity["activityName"]
+                garmin_name = garmin_activity["activityName"]
+                if strava_start == garmin_start and strava_name != garmin_name:
                     self.set_strava_activity_name(strava_activity["id"], garmin_name)
-                    print(f"Found activity at {garmin_start}, synced name to {garmin_name}")
+                    print(
+                        f"Activity at {garmin_start}: changed Strava activity name from {strava_name} to {garmin_name}"
+                    )
                     break
 
-        return
 
+load_dotenv()
 
-access_token = os.getenv("SYNC_ACCESS_TOKEN")
-refresh_token = os.getenv("SYNC_REFRESH_TOKEN")
-sync = Synchronizer(access_token, refresh_token)
-sync.set_strava_activity_name(11696146089, "hohiho")
+sync = Synchronizer(load_strava_token())
+
+after_time = int((datetime.now() - timedelta(days=4)).timestamp())
+
+sync.sync_activity_name_from_garmin_to_strava(
+    garmin_activities=sync.get_garmin_activities(0, 4),
+    strava_activities=sync.get_strava_activities(after=after_time)
+)
